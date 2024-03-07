e853543e658c089216ca82dd35668be2d71d2b63
pilarpsico <pillastraead@gmail.com>
2024-03-07 17:34:57 -0300

    cica

---
+++ script/cica.js
@@ -0,0 +1,8 @@
+var nome = prompt("qual é seu nome?");
+alert(nome + " , oieee, bem vinda!");
+
+var aniversario = prompt("Fez Aniversário, né?");
+alert("Parabénssss " + nome + "!");
+
+var desejo = prompt("Faça um desejo!!!");
+alert("Acredite! " + desejo + " já está em seu caminho!")
