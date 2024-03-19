ed4dd3ca817f427e33ba83cf5397eeafbbd9f717
pilarpsico <pillastraead@gmail.com>
2024-03-19 10:27:05 -0300

    frasemotivacional

---
+++ script/motivacional.js
@@ -0,0 +1,16 @@
+const respostas = [
+    "A vida é feita de pequenos momentos de felicidade",
+    "Todo novo acordar é um presente",
+    "Acredite que você é muito forte",
+    "Você é um ser humano lindo que merece muito amor",
+]
+function gerarFrase(){
+
+const totalRespostas = respostas.length
+
+const numeroAleatorio = Math.floor(Math.random()*totalRespostas)
+
+const elementoResposta = document.querySelector("#resposta")
+
+elementoResposta.innerHTML = respostas[numeroAleatorio]
+}
